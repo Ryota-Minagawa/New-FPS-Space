42f1802499930cd09bda36275c6c87727b327ef1
Ryota-Minagawa <ryota19961023@icloud.com>
2018-06-24 17:16:14 +0900

    [add] ターゲットを配置して5回打ったら倒れるようにしました。また５秒後に起き上がるようにして、targetMarkerの中心に近いほど高い得点が得られるようにしました。

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+    
+    public float score;     Vector3 centor;
+    GameObject targetMarker;
+
+	// Use this for initialization
+	void Start () {
+        targetMarker=GameObject.Find("TargetMarker");
+        centor = targetMarker.GetComponent<Renderer>().bounds.center;
+	}
+	
+    public void AddScore(Vector3 hitPoint)
+    {         float distance = Vector3.Distance(centor, hitPoint);         score += 100.0f - (distance * 200.0f);     } }
---
+++ Assets/Scripts/TargetManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class TargetManager : MonoBehaviour {
+
+    Animator animator;
+    BoxCollider boxCollider;
+    public int hp = 5;
+
+	// Use this for initialization
+	void Start () {
+        animator = gameObject.GetComponent<Animator>();
+        boxCollider = gameObject.GetComponent<BoxCollider>();
+
+        this.UpdateAsObservable()
+            .Where(_ => hp == 0)
+            .ThrottleFirst(TimeSpan.FromSeconds(5))
+            .Subscribe(_ => Invoke("WakeUp", 5));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (hp == 0)         {             animator.SetBool("broken", true);             boxCollider.enabled = false;         }
+	}
+    void WakeUp()     {         animator.SetBool("broken", false);         boxCollider.enabled = true;         hp = 5;     } 
+    public void DeclineHp(){
+        --hp;
+    }
+}
