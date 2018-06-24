56ff9f5be44939f9406f2c1089de7443edb8679d
Ryota-Minagawa <ryota19961023@icloud.com>
2018-06-24 18:43:37 +0900

    [add]右クリックを押すとスナイパーモードになるように設定しました

---
+++ Assets/Scripts/SniperReticleScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperReticleScript : MonoBehaviour
+{
+
+    [SerializeField] GameObject sniperReticle;
+    [SerializeField] Camera firstPersonCamera;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (sniperReticle.activeSelf)
+            {
+                sniperReticle.SetActive(false);
+                firstPersonCamera.fieldOfView = 60;
+            }
+            else
+            {
+                sniperReticle.SetActive(true);
+                firstPersonCamera.fieldOfView = 20;
+            }
+        }
+    }
+}
+
