8537a2352f59541c1e6ad37e4d787579116953ee
Ryota-Minagawa <ryota19961023@icloud.com>
2018-07-03 13:38:11 +0900

    [add] Matchingシーンを作成し、Buttonを押したらゲームのシーンに移動できるようにしました。

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ButtonController : MonoBehaviour {
+
+    public void ClickButton()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+}
